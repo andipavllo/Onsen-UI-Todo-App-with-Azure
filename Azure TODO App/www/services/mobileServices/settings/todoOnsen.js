e7f5432b851de90e4607c030505e94e20d191da1
Andi Pavllo <andi.pavllo@gmail.com>
2015-06-23 14:34:48 +0900

    Update todoOnsen.js

--- Azure TODO App/www/services/mobileServices/settings/todoOnsen.js
+++ Azure TODO App/www/services/mobileServices/settings/todoOnsen.js
@@ -1,5 +1,5 @@
 ﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x409
 var todoOnsenClient = new WindowsAzure.MobileServiceClient(
-                        "https://onsen-test.azure-mobile.net/,
+                        "https://onsen-test.azure-mobile.net/",
                         "RGqXOaNzOwKQvmnXukWHnzRHnCfeSO18"
 );
